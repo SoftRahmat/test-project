2f888fb2855ca7e1de11588f1dd92d7e3c45ba2d
SoftRahmat <rahmatapon402@gmail.com>
2023-06-16 21:29:27 +0800

    Implemented search option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ArticleInterface } from './article.interface';
+import { ArticlesService } from './articles.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'test-project';
+export class AppComponent implements OnInit {
+  searchValue = '';
+  articles: ArticleInterface[] = [];
+  searchForm = this.fb.group({
+    searchValue: '',
+  });
+
+  constructor(
+    private articlesService: ArticlesService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    this.articlesService.getArticles(this.searchValue).subscribe((posts) => {
+      this.articles = posts;
+    });
+  }
+
+  onSearchSubmit(): void {
+    this.searchValue = this.searchForm.value.searchValue ?? '';
+    this.fetchData();
+  }
 }
---
+++ src/app/article.interface.ts
@@ -0,0 +1,6 @@
+export interface ArticleInterface {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
